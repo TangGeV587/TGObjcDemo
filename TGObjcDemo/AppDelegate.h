af37154708c9926077f1eaa9c23061508669083f
e-zhaoyutang <e-zhaoyutang@geely.com>
2022-03-24 16:11:21 +0800

    Initial Commit

---
+++ TGObjcDemo/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  TGObjcDemo
+//
+//  Created by e-zhaoyutang on 2022/3/24.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ TGObjcDemo/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  TGObjcDemo
+//
+//  Created by e-zhaoyutang on 2022/3/24.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
